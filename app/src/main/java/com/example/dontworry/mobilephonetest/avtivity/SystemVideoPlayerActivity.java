3144a0c52b12ebf7b69eb93f815429f31e15e2be
saygogo <1183769411@qq.com>
2017-05-22 23:59:52 +0800

    播放进度条的调整

--- app/src/main/java/com/example/dontworry/mobilephonetest/avtivity/SystemVideoPlayerActivity.java
+++ app/src/main/java/com/example/dontworry/mobilephonetest/avtivity/SystemVideoPlayerActivity.java
@@ -1,9 +1,16 @@
 package com.example.dontworry.mobilephonetest.avtivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,13 +22,16 @@ import android.widget.VideoView;
 
 import com.example.dontworry.mobilephonetest.R;
 import com.example.dontworry.mobilephonetest.bean.MediaItem;
+import com.example.dontworry.mobilephonetest.utils.Utils;
 
-import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class SystemVideoPlayerActivity extends AppCompatActivity implements View.OnClickListener {
     private VideoView vv;
     private Uri uri;
+    private static final int PROGRESS = 0;
 
     private LinearLayout llTop;
     private TextView tvName;
@@ -40,66 +50,96 @@ public class SystemVideoPlayerActivity extends AppCompatActivity implements View
     private Button btnNext;
     private Button btnSwitchScreen;
     private ArrayList<MediaItem> videolist;
+    private Utils utils;
+    private MyBroadCastReceiver receiver;
 
     private void findViews() {
         setContentView(R.layout.activity_system_video_player);
-        llTop = (LinearLayout)findViewById( R.id.ll_top );
-        tvName = (TextView)findViewById( R.id.tv_name );
-        ivBattery = (ImageView)findViewById( R.id.iv_battery );
-        tvSystemTime = (TextView)findViewById( R.id.tv_system_time );
-        btnVoice = (Button)findViewById( R.id.btn_voice );
-        seekbarVoice = (SeekBar)findViewById( R.id.seekbar_voice );
-        btnSwitchPlayer = (Button)findViewById( R.id.btn_switch_player );
-        llBottom = (LinearLayout)findViewById( R.id.ll_bottom );
-        tvCurrentTime = (TextView)findViewById( R.id.tv_current_time );
-        seekbarVideo = (SeekBar)findViewById( R.id.seekbar_video );
-        tvDuration = (TextView)findViewById( R.id.tv_duration );
-        btnExit = (Button)findViewById( R.id.btn_exit );
-        btnPre = (Button)findViewById( R.id.btn_pre );
-        btnStartPause = (Button)findViewById( R.id.btn_start_pause );
-        btnNext = (Button)findViewById( R.id.btn_next );
-        btnSwitchScreen = (Button)findViewById( R.id.btn_switch_screen );
-        vv = (VideoView)findViewById(R.id.vv);
+        llTop = (LinearLayout) findViewById(R.id.ll_top);
+        tvName = (TextView) findViewById(R.id.tv_name);
+        ivBattery = (ImageView) findViewById(R.id.iv_battery);
+        tvSystemTime = (TextView) findViewById(R.id.tv_system_time);
+        btnVoice = (Button) findViewById(R.id.btn_voice);
+        seekbarVoice = (SeekBar) findViewById(R.id.seekbar_voice);
+        btnSwitchPlayer = (Button) findViewById(R.id.btn_switch_player);
+        llBottom = (LinearLayout) findViewById(R.id.ll_bottom);
+        tvCurrentTime = (TextView) findViewById(R.id.tv_current_time);
+        seekbarVideo = (SeekBar) findViewById(R.id.seekbar_video);
+        tvDuration = (TextView) findViewById(R.id.tv_duration);
+        btnExit = (Button) findViewById(R.id.btn_exit);
+        btnPre = (Button) findViewById(R.id.btn_pre);
+        btnStartPause = (Button) findViewById(R.id.btn_start_pause);
+        btnNext = (Button) findViewById(R.id.btn_next);
+        btnSwitchScreen = (Button) findViewById(R.id.btn_switch_screen);
+        vv = (VideoView) findViewById(R.id.vv);
 
-        btnVoice.setOnClickListener( this );
-        btnSwitchPlayer.setOnClickListener( this );
-        btnExit.setOnClickListener( this );
-        btnPre.setOnClickListener( this );
-        btnStartPause.setOnClickListener( this );
-        btnNext.setOnClickListener( this );
-        btnSwitchScreen.setOnClickListener( this );
+        btnVoice.setOnClickListener(this);
+        btnSwitchPlayer.setOnClickListener(this);
+        btnExit.setOnClickListener(this);
+        btnPre.setOnClickListener(this);
+        btnStartPause.setOnClickListener(this);
+        btnNext.setOnClickListener(this);
+        btnSwitchScreen.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if ( v == btnVoice ) {
+        if (v == btnVoice) {
             // Handle clicks for btnVoice
-        } else if ( v == btnSwitchPlayer ) {
+        } else if (v == btnSwitchPlayer) {
             // Handle clicks for btnSwitchPlayer
-        } else if ( v == btnExit ) {
+        } else if (v == btnExit) {
             // Handle clicks for btnExit
-        } else if ( v == btnPre ) {
+        } else if (v == btnPre) {
             // Handle clicks for btnPre
-        } else if ( v == btnStartPause ) {
-            if(vv.isPlaying()){
+        } else if (v == btnStartPause) {
+            if (vv.isPlaying()) {
                 vv.pause();
                 btnStartPause.setBackgroundResource(R.drawable.btn_start_selector);
-            }else {
+            } else {
                 vv.start();
                 btnStartPause.setBackgroundResource(R.drawable.btn_pause_selector);
             }
             // Handle clicks for btnStartPause
-        } else if ( v == btnNext ) {
+        } else if (v == btnNext) {
             // Handle clicks for btnNext
-        } else if ( v == btnSwitchScreen ) {
+        } else if (v == btnSwitchScreen) {
             // Handle clicks for btnSwitchScreen
         }
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case PROGRESS:
+                    //得到当前进度
+                    int currentPosition = vv.getCurrentPosition();
+                    //让SeekBar进度更新
+                    seekbarVideo.setProgress(currentPosition);
+
+                    //设置文本当前的播放进度
+                    tvCurrentTime.setText(utils.stringForTime(currentPosition));
+
+                    //循环发消息
+                    sendEmptyMessageDelayed(PROGRESS, 1000);
+
+                    break;
+            }
+        }
+    };
+
+    private String getSystemTime() {
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        return format.format(new Date());
     }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initDate();
 
         findViews();
         videolist = (ArrayList<MediaItem>) getIntent().getSerializableExtra("videolist");
@@ -109,13 +149,58 @@ public class SystemVideoPlayerActivity extends AppCompatActivity implements View
 
     }
 
+    private void initDate() {
+        utils = new Utils();
+        //注册监听电量变化广播
+        receiver = new MyBroadCastReceiver();
+        IntentFilter intentFilter  = new IntentFilter();
+        //监听电量变化
+        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(receiver,intentFilter);
+    }
+
+
+
+    class MyBroadCastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra("level", 0);//主线程
+            Log.e("TAG","level=="+level);
+            setBatteryView(level);
+
+        }
+    }
+
+    private void setBatteryView(int level) {
+        if(level <=0){
+            ivBattery.setImageResource(R.drawable.ic_battery_0);
+        }else if(level <= 10){
+            ivBattery.setImageResource(R.drawable.ic_battery_10);
+        }else if(level <=20){
+            ivBattery.setImageResource(R.drawable.ic_battery_20);
+        }else if(level <=40){
+            ivBattery.setImageResource(R.drawable.ic_battery_40);
+        }else if(level <=60){
+            ivBattery.setImageResource(R.drawable.ic_battery_60);
+        }else if(level <=80){
+            ivBattery.setImageResource(R.drawable.ic_battery_80);
+        }else if(level <=100){
+            ivBattery.setImageResource(R.drawable.ic_battery_100);
+        }else {
+            ivBattery.setImageResource(R.drawable.ic_battery_100);
+        }
+    }
+
     private void setListener() {
         vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
                 int duration = vv.getDuration();
+                tvDuration.setText(utils.stringForTime(duration));
                 seekbarVideo.setMax(duration);
                 vv.start();
+                handler.sendEmptyMessage(PROGRESS);
             }
         });
 
@@ -138,7 +223,7 @@ public class SystemVideoPlayerActivity extends AppCompatActivity implements View
         seekbarVideo.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if(fromUser){
+                if (fromUser) {
                     vv.seekTo(progress);
                 }
 
@@ -154,6 +239,20 @@ public class SystemVideoPlayerActivity extends AppCompatActivity implements View
 
             }
         });
+    }
+    @Override
+    protected void onDestroy() {
+
+        if(handler != null){
+            handler.removeCallbacksAndMessages(null);
+            handler = null;
+        }
+
+        if(receiver != null){
+            unregisterReceiver(receiver);
+            receiver = null;
+        }
+        super.onDestroy();
     }
 }
 
